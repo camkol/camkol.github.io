9fbe323a902534cbcf06a410e1103404d389860f
camkol <koliyus3@hotmail.com>
2023-09-09 23:41:21 +0200

    revamp titles

---
+++ mc3.js
@@ -0,0 +1,14 @@
+var wordToDisplay = "Education";
+
+var blue = [28, 149, 248];
+var white = [255, 255, 255];
+var black = [0, 0, 0];
+var purple = [255, 55, 245];
+var yellow = [280, 50, 60];
+var letterColors = [blue, white, black, purple, yellow];
+
+drawName(wordToDisplay, letterColors);
+
+bubbleShape = 'circle';
+
+bounceBubbles();
