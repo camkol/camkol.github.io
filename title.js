dc5b3a277ac3183191379c4be5a289067d4e92c9
camkol <121874225+camkol@users.noreply.github.com>
2024-02-22 17:42:50 +0100

    - added marquee for icons - installed incredile title function

--- title.js
+++ title.js
@@ -1,61 +1,52 @@
-var t2 = new SplitText(".txt2").chars,
-  color2 = "#17c0fd",
-  color1 = "#fff",
-  moveBar = () => {
-    gsap.set(".bar", { left: gsap.getProperty(".txt1", "width") + 1 });
-  };
+const words = ["Frontend", "Web", "Junior", "Entry-Level"],
+  colors = ["blue", "green", "yellow", "red"],
+  text = document.querySelector(".text");
 
-gsap
-  .timeline({ delay: 0.2 })
-  .set(".txt1", { color: color1, fontWeight: "regular" })
-  .set(".txt2", {
-    color: color2,
-    fontWeight: "bold",
-    opacity: 0,
-    x: gsap.getProperty(".txt1", "width") - 2,
-    immediateRender: true,
-  })
-  .set(".bar", { left: 1, backgroundColor: color1, immediateRender: true })
+// Generator (iterate from 0-3)
+function* generator() {
+  var index = 0;
+  while (true) {
+    yield index++;
 
-  .to(
-    ".bar",
-    {
-      duration: 0.1,
-      opacity: 0,
-      ease: Expo.easeIn,
-      yoyo: true,
-      repeat: 5,
-      repeatDelay: 0.3,
-    },
-    0
-  )
-  .from(
-    ".txt1",
-    {
-      duration: 1.1,
-      width: 0,
-      ease: SteppedEase.config(14),
-      onUpdate: moveBar,
-    },
-    2.5
-  )
-  .to(".bar", { duration: 0.05, backgroundColor: color2 }, "+=0.15")
-  .to(".bar", { duration: 1.0, width: 290, ease: Power4.easeInOut }, "+=0.1")
-  .from(
-    ".containerTitle",
-    { duration: 1.0, x: 135, ease: Power4.easeInOut },
-    "-=1.0"
-  )
-  .to(".txt2", { duration: 0.01, opacity: 1 }, "-=0.1")
-  .to(".bar", { duration: 0.4, x: 290, width: 0, ease: Power4.easeIn })
-  .from(
-    t2,
-    { duration: 0.6, opacity: 0, ease: Power3.easeInOut, stagger: 0.02 },
-    "-=0.5"
-  )
-  .to(
-    ".txt1",
-    { duration: 1.5, opacity: 0.75, ease: Power3.easeInOut },
-    "-=1.2"
-  )
-  .timeScale(1.45);
+    if (index > 3) {
+      index = 0;
+    }
+  }
+}
+
+// Printing effect
+function printChar(word) {
+  let i = 0;
+  text.innerHTML = "";
+  text.classList.add(colors[words.indexOf(word)]);
+  let id = setInterval(() => {
+    if (i >= word.length) {
+      deleteChar();
+      clearInterval(id);
+    } else {
+      text.innerHTML += word[i];
+      i++;
+    }
+  }, 500);
+}
+
+// Deleting effect
+function deleteChar() {
+  let word = text.innerHTML;
+  let i = word.length - 1;
+  let id = setInterval(() => {
+    if (i >= 0) {
+      text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
+      i--;
+    } else {
+      text.classList.remove(colors[words.indexOf(word)]);
+      printChar(words[gen.next().value]);
+      clearInterval(id);
+    }
+  }, 300);
+}
+
+// Initializing generator
+let gen = generator();
+
+printChar(words[gen.next().value]);
